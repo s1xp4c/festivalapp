1f2d83e97353472ee95e01801294e89e20819d32
s1xp4c <blockstarterinfo@gmail.com>
2023-10-23 13:48:08 +0200

    Build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 import HeroSection from ":/components/heroSection/HeroSection";
 import MaxWidthWrapper from ":/components/ui/MaxWidthWrapper";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-20 flex flex-col items-center justify-center text-center">
